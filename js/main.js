afb2a4b59e545c8709c59100c5c52acdde72f888
nickhaugen19 <71619276+nickhaugen19@users.noreply.github.com>
2023-02-28 14:50:22 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -49,7 +49,56 @@ d3.csv("https://raw.githubusercontent.com/DS4200-S23-Class/hw-06-ethan-nick/mast
 });
 
 
-var svg2 = d3.select("#dataviz3")
+var svg = d3.select("#middle")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+d3.csv("https://raw.githubusercontent.com/DS4200-S23-Class/hw-06-ethan-nick/master/data/iris.csv", function(data) {
+
+  var x = d3.scaleLinear()
+    .domain([0, 5.0])
+    .range([ 0, width ]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  var y = d3.scaleLinear()
+    .domain([0, 3.0])
+    .range([ height, 0]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+  var circles = svg.append('g')
+  	.attr("id", "plane")
+    .selectAll("point")
+    .data(data)
+    .enter()
+    .append("circle")   	
+      .attr("cx", function (d) { return x(d.Petal_Width); } )
+      .attr("cy", function (d) { return y(d.Sepal_Width); } )
+      .attr("r", 5)
+      .attr("fill-opacity", 0.5)
+      .attr("fill", function(d) {
+        if (d.Species === "setosa") {
+            return "#DC267F";
+        } else if (d.Species === "versicolor") {
+            return "#FE6100";
+        } else if (d.Species === "virginica") {
+            return "#FFB000";
+        }
+          
+        });
+
+});
+
+
+
+
+var svg2 = d3.select("#right")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
