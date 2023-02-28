1d2ab81bf2e7617b232d0395bf5dc32842b02dca
nickhaugen19 <71619276+nickhaugen19@users.noreply.github.com>
2023-02-28 00:06:28 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var margin = {top: 10, right: 30, bottom: 30, left: 60},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
-var svg = d3.select("#my_dataviz")
+var svg = d3.select("#dataviz1")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -35,45 +35,6 @@ d3.csv("https://raw.githubusercontent.com/DS4200-S23-Class/hw-05-hw05-nick-ethan
       .attr("cy", function (d) { return y(d.y); } )
       .attr("r", 15)
       .style("fill", "steelblue")
-
-  var button = d3.select("#plotbutton")
-		button.on("click", function(d){
-		var newx = d3.select("#xdropdown").node().value;
-		var newy = d3.select("#ydropdown").node().value;
-		d3.select("#plane")
-		.append("circle")
-	      .attr("cx", function (d) { return x(newx); } )
-     	  .attr("cy", function (d) { return y(newy); } )
-	      .attr("r", 15)
-	      .style("fill", "steelblue")
-	      .on("mousemove", function(d) {
-      			d3.select(this)
-          		.style("cursor", "pointer")
-          		.style("fill", "orange");
-   			  })
-   			
-  		  .on("mouseout", function(d) {
-    		    d3.select(this)
-       			.style("cursor", "default")
-        		.style("fill", "steelblue");
-    		  })
-      
-  		  .on("click", function(d) {
-      			var selectedCircle = d3.select(this);
-      			var isSelected = selectedCircle.classed("selected");
-      			selectedCircle.classed("selected", !isSelected);
-     			coords.text("(" + newx + ", " + newy + ")");
-  
-  				// toggle stroke color and width
-			      if (isSelected) {
-			          selectedCircle
-			              .style("stroke", null)
-			              .style("stroke-width", null);
-			      } else {
-			          selectedCircle
-			              .style("stroke", "black")
-			              .style("stroke-width", "7px");
-			      }});
 
 });
 
